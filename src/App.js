908c781f8e68ced1d2dc34814ad18903cf30abec
Adam <ashaw315@gmail.com>
2022-01-25 10:01:48 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
@@ -8,13 +8,34 @@ import Home from './components/Home';
 import Listings from './components/Listings';
 import './App.css';
 
+
 function App() {
+
+  const [userData, setUserData] = useState([])
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    fetch('http://localhost:9292/users')
+      .then((r) => r.json())
+      .then((userData) => setUserData(userData))
+  },[])
+
+  function handleLogin(name, password, status){
+    setUserName(name)
+    setLoggedIn(status)
+    setPassword(password)
+  }
+
+  
   return (
     <div className="App">
       <Navbar/>
       <Routes>
         <Route/>
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={
+        <Home handleLogin={handleLogin} userData={userData} />} /> 
         <Route path="/listings" element={<Listings />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/calendar" element={<Calendar/>} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Home() {
+function Home( userData, userInfo ) {
+const [userName, setUserName] = useState('')
+const [pass, setPass] = useState('')
+const [details, setDetails] = useState({name: "", password: ""});
+
+const {name, password} = userData
+
+
+
+const submitHandler = e => {
+    e.preventDefault();
+    userData(details)
+}
+
+console.log(userData)
+
     return (
         <div>
+          <form onSubmit={submitHandler}>
             <h1>This is the Home</h1>
+            <div className="form-inner">
+                <h2>Login</h2>
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name}></input>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password}></input>
+                </div>
+                <button value="submit" value="LOGIN">LOGIN</button>
+            </div>
+          </form>
         </div>
     )
 }
