8a776a839a5e222d5a1b19208491909596b0512d
Adam <ashaw315@gmail.com>
2022-01-29 13:17:28 -0500

    added reviews

--- src/components/DetailCard.js
+++ src/components/DetailCard.js
@@ -4,9 +4,33 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import styled from "styled-components"
+
+const InputA = styled.input`
+    background: white;
+    padding: 10px;
+    margin-right: 20px;
+    border: 2px grey;
+    transition: 500ms ease;
+    color: black;
+    text-decoration: none;
+    font: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    border: 2px solid rgba(0, 0, 0, 1);
+    border-radius: 10px;
+
+    
+
+    &:hover {
+        background: grey;
+        color: white;
+        border: 2px solid rgba(0, 0, 0, 1);
+        border-radius: 10px;
+    }
+    `;
 
 
 function DetailCard(){
+const { id } = useParams();
 const [listing, setListing] = useState(null)
 const [isLoaded, setIsLoaded] = useState(false);
 const [comments, setComments] = useState(null)
@@ -14,7 +38,7 @@ const [formData, setFormData] = useState({
   review_name: "",
   comment: "",
   rating: 0, 
-  listing: ""
+  listing: id
 })
 
 
@@ -29,14 +53,9 @@ function handleSubmit(e) {
     review_name: "",
     comment: "",
     rating: 0,
-    listing: ""
+    listing: id
   })
 }
-
-
-
-
-const { id } = useParams();
 
   useEffect(() => {
     fetch(`http://localhost:9292/listings/${id}`)
@@ -47,34 +66,30 @@ const { id } = useParams();
     });
 }, [id]);
 
+  useEffect(() => {
+    fetch('http://localhost:9292/reviews')
+    .then(r => r.json())
+    .then(data => setComments(data))
+  }, [])
 
-
-useEffect(() => {
-  fetch('http://localhost:9292/reviews')
+  const postReview = (review) => {
+  fetch('http://localhost:9292/reviews', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(review)
+  })
   .then(r => r.json())
-  .then(data => setComments(data))
-}, [])
-
-const postReview = (review) => {
-fetch('http://localhost:9292/reviews', {
-  method: 'POST',
-  headers: {
-      'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(review)
-})
-.then(r => r.json())
-.then(newReview => {
-  setComments([newReview,...comments])
-})
-}
+  .then(newReview => {
+    setComments([newReview,...comments])
+  })
+  }
 
 
 if (!isLoaded) return <h2>Loading...</h2>;
 
 const {job_title, hourly_rate, start_date, end_date, job_description, reviews, rating} = listing
-
-const postId = listing.id
 
 const path = `/users/${id}`;
 
@@ -86,14 +101,14 @@ const all_reviews = listing.reviews.map((review) => {
                   aria-controls="panel1a-content"
                   id="panel1a-header"
                   >
-                  <Typography><strong>{review.user.name}</strong></Typography>
+                  <Typography><strong>{review.user ? review.user.name : "Anonymous" }</strong></Typography>
                   </AccordionSummary>
                   <AccordionDetails>
                     <Typography>
-                      <p>Current Position: {review.user.position}</p>
-                      <p>Skill: {review.user.user_despcription}</p>  
-                      <p>Hobby: {review.user.skills}</p>  
-                      <p>Favorite Fruit: {review.user.password}</p>    
+                      <p>Current Position: {review.user ? review.user.position : "Art Handler"}</p>
+                      <p>Skill: {review.user ? review.user.user_despcription : "Art management"}</p>  
+                      <p>Hobby: {review.user ? review.user.skills : "Leveling"}</p>  
+                      <p>Favorite Fruit: {review.user ? review.user.password : "Apple"}</p>    
                     </Typography>
                   </AccordionDetails>
               </Accordion>
@@ -127,14 +142,15 @@ const all_reviews = listing.reviews.map((review) => {
   })
 
     return (
-    <section className="whole-detail">
-      <div className="project-image">
+    <section >
+      <div className="whole-detail">
         <h1>{job_title}</h1>
-        </div>
-        <div>
+        
+        
           <p>{start_date}</p>
           <p>${hourly_rate}</p>
           <p>{job_description}</p>
+          </div>
           {/* <p  className='test-position'>{all_reviews}</p> */}
           <Accordion className="accordian-detail">
             <AccordionSummary
@@ -162,9 +178,40 @@ const all_reviews = listing.reviews.map((review) => {
               </Typography>
             </AccordionDetails>
            </Accordion>
-           </div>
-          <div>
-      </div>
+           
+          
+      
+      <form className="new-review" onSubmit={handleSubmit}>
+                    <label>
+                    Full Name:
+                    </label>
+                     <input
+                    type="text"
+                    name="review_name"
+                    value={formData.review_name}
+                    onChange={handleChange}
+                    />
+                    <label>
+                    Rating:
+                    </label>
+                     <input
+                    type="text"
+                    name="rating"
+                    value={formData.rating}
+                    onChange={handleChange}
+                    />
+                     <label>
+                    Comment:
+                    </label>
+                     <input
+                    type="text"
+                    name="comment"
+                    value={formData.comment}
+                    onChange={handleChange}
+                    />
+                    <br />
+                <InputA type="submit" value="Submit" />
+        </form>
   </section>
     )
 }
